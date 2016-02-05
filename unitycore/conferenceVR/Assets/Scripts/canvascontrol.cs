380547ffd30c555d646b77f51b72315d1d09f810
sirerr <sirerr@gmail.com>
2016-02-04 22:05:33 -0500

    big push

---
+++ unitycore/conferenceVR/Assets/Scripts/canvascontrol.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class canvascontrol : MonoBehaviour {
+
+	public GameObject controlboxobj;
+	public Button controlboxbutton;
+	public GameObject filesharebuttonobj;
+	public Button filesharebutton;
+
+	void Awake()
+	{
+		controlboxobj = transform.GetChild(0).transform.gameObject;
+		controlboxbutton = controlboxobj.GetComponent<Button>();
+		controlboxbutton.onClick.AddListener(delegate {enablebutton();});
+		filesharebuttonobj = transform.GetChild(1).transform.gameObject;
+		filesharebutton = transform.GetChild(1).transform.GetComponent<Button>();
+					
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void enablebutton()
+
+	{
+		filesharebuttonobj.SetActive(true);
+
+		print("I am working");
+	}
+}
---
+++ unitycore/conferenceVR/Assets/Scripts/usercontrol.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class usercontrol : MonoBehaviour {
+
+	//the control box canvas
+	public GameObject usercanvas;
+	//the user cursor
+	public GameObject usercursor;
+	//
+	private Vector3 cursorloc;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetMouseButtonUp(1))
+		{
+		//	cursorloc = usercursor.transform.position;
+		//	usercanvas.transform.position = new Vector3(cursorloc.x,cursorloc.y,cursorloc.z+.5f);
+		//	usercanvas.transform.rotation = usercursor.transform.rotation;
+		//	usercanvas.transform.LookAt(new Vector3(cursorloc.x,cursorloc.y,cursorloc.z));
+			if(usercanvas.activeSelf)
+			{
+				usercanvas.SetActive(false);
+			}else
+			{
+				usercanvas.SetActive(true);
+			}
+
+		}
+	}
+}
